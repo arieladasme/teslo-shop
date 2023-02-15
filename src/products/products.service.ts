6ea7d447c11917dbb6913329efae3c7668f02257
ariel <ariel.wm1@gmail.com>
2023-02-15 15:55:23 -0300

    transactions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -90,11 +90,29 @@ export class ProductsService {
 
     // Create query runner
     const queryRunner = this.dataSource.createQueryRunner()
+    await queryRunner.connect()
+    await queryRunner.startTransaction()
 
     try {
-      await this.productRepository.save(product)
-      return product
+      // borro imagenes si es que existen para agregar las nuevas
+      // delete from product_image where producID = id
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } })
+        product.images = images.map((image) => this.productImageRepository.create({ url: image }))
+      } else {
+      }
+
+      await queryRunner.manager.save(product)
+      // await this.productRepository.save(product)
+
+      // finalizo queryRunner
+      await queryRunner.commitTransaction()
+      await queryRunner.release()
+
+      return this.findOnePlain(id)
     } catch (error) {
+      await queryRunner.rollbackTransaction()
+      await queryRunner.release()
       this.handleDBExceptions(error)
     }
   }
