699e9eae440f09458d07a0233c60af1da963b7f4
ariel <ariel.wm1@gmail.com>
2023-02-20 15:12:17 -0300

    deploy images 2

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,6 +8,7 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { Response } from 'express'
 import { diskStorage } from 'multer'
@@ -16,7 +17,10 @@ import { fileFilter, fileNamer } from './helpers'
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(@Res() res: Response, @Param('imageName') imageName: string) {
@@ -35,8 +39,8 @@ export class FilesController {
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('Make sure that the file is an image')
 
-    const secureUrl = `http://localhost:3000/api/files/product/503b167c-b966-4623-a727-08440beaede8.png`
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`
 
-    return secureUrl
+    return { secureUrl }
   }
 }
