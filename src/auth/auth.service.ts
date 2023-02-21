fb198c2e9aacb702e99fa18ac957a92c265fa689
ariel <ariel.wm1@gmail.com>
2023-02-21 12:24:22 -0300

    user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import * as bcrypt from 'bcrypt'
-import { CreateUserDto } from './dto/create-user.dto'
+import { CreateUserDto, LoginUserDto } from './dto'
 import { User } from './entities/user.entity'
 
 @Injectable()
@@ -28,6 +33,20 @@ export class AuthService {
     } catch (error) {
       this.handleDBErrors(error)
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    })
+
+    if (!user || !bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid')
+
+    return user // TODO: return jwt
   }
   //:never -> metodo que nunca retornara algo
   private handleDBErrors(error: any): never {
