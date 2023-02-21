9e0ee4839b3520da6c120181afd9a02f15b473b6
ariel <ariel.wm1@gmail.com>
2023-02-21 12:54:00 -0300

    async module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
@@ -6,12 +7,22 @@ import { User } from './entities/user.entity'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
+//console.log(process.env.JWT_SECRET)
+//console.log(configService.get('JWT_SECRET'))
+
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return { secret: configService.get('JWT_SECRET'), signOptions: { expiresIn: '2h' } }
+      },
+    }),
     // JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '2h' } }),
   ],
   exports: [TypeOrmModule],
