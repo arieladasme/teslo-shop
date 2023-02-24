44ad7c655ad6aa61cba1ab8e26be672146a91656
ariel <ariel.wm1@gmail.com>
2023-02-24 13:02:36 -0300

    import @Auth()

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,12 +13,16 @@ import { ProductsService } from './products.service'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
 import { PaginationDto } from 'src/common/dtos/pagination.dto'
+import { Auth } from 'src/auth/decorators'
+import { validRoles } from 'src/auth/interfaces'
 
 @Controller('products')
+@Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(validRoles.user)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto)
   }
@@ -34,11 +38,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(validRoles.adim)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto)
   }
 
   @Delete(':id')
+  @Auth(validRoles.adim)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id)
   }
